2730147c47bf651594f298492bf9347e06ee0e4f
Andivyy11 <andivyy@gmail.com>
2024-09-22 17:04:38 +0530

    done

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const path = require('path');
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload = multer({ dest: 'uploads/' }); // Stores files in 'uploads' directory
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file;
+
+  if (!file) {
+    return res.status(400).send("No file uploaded.");
+  }
+  res.json({
+    name:file.originalname,
+    type:file.mimetype,
+    size:file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
